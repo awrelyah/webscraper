3c1a20913eb8b03b175daa203e9a24b54658cb14
awrelyah <aureliamihkk@gmail.com>
2022-09-20 11:15:05 +0300

    scrape last 10 comics

--- scrape.php
+++ scrape.php
@@ -7,17 +7,20 @@ if(!is_dir('./cache')){
 }
 
 $client = new GuzzleHttp\CLient(['verify' => false]);
-for($i = 610; $i > 600; $i--){
+$url = 'https://abstrusegoose.com/';
+for($i = 0 ; $i < 10 ; $i++){
     if(file_exists('./cache/'. $i . 'html')){
         $html = file_get_contents('./cache/'. $i . 'html');
     } else {
-        $response = $client->get('https://abstrusegoose.com/'. $i);
+        $response = $client->get($url);
         $html = $response->getBody()->getContents();
         file_put_contents('./cache/'. $i . 'html', $html);
     }
     $crawler = new \Symfony\Component\DomCrawler\Crawler($html);
-    $imgEl = $crawler->filter('section img');
+    $imgEl = $crawler->filter('section>img');
     var_dump($imgEl->attr('src'));
     var_dump($imgEl->attr('title'));
     var_dump($imgEl->attr('alt'));
+    $link = $crawler->filter('body>section>p:nth-child(1)>a:nth-child(2)');
+    $url = $link->attr('href');
 }
