62d1bf4afa5d859b908a5f877d28dbeefea9af2e
awrelyah <aureliamihkk@gmail.com>
2022-09-20 10:55:58 +0300

    get 10 comics

--- scrape.php
+++ scrape.php
@@ -2,12 +2,22 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-$client = new GuzzleHttp\CLient(['verify' => false]);
-$response = $client->get('https://abstrusegoose.com');
-$html = $response->getBody()->getContents();
-$crawler = new \Symfony\Component\DomCrawler\Crawler($html);
-$imgEl = $crawler->filter('section img');
+if(!is_dir('./cache')){
+    mkdir('./cache');
+}
 
-var_dump($imgEl->attr('src'));
-var_dump($imgEl->attr('title'));
-var_dump($imgEl->attr('alt'));
+$client = new GuzzleHttp\CLient(['verify' => false]);
+for($i = 610; $i > 600; $i--){
+    if(file_exists('./cache/'. $i . 'html')){
+        $html = file_get_contents('./cache/'. $i . 'html');
+    } else {
+        $response = $client->get('https://abstrusegoose.com/'. $i);
+        $html = $response->getBody()->getContents();
+        file_put_contents('./cache/'. $i . 'html', $html);
+    }
+    $crawler = new \Symfony\Component\DomCrawler\Crawler($html);
+    $imgEl = $crawler->filter('section img');
+    var_dump($imgEl->attr('src'));
+    var_dump($imgEl->attr('title'));
+    var_dump($imgEl->attr('alt'));
+}
