2464e1b44c3a58285ca11c68f09e17cb53c7f220
awrelyah <aureliamihkk@gmail.com>
2022-09-20 10:27:18 +0300

    get one comic

--- scrape.php
+++ scrape.php
@@ -1,1 +1,13 @@
 <?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$client = new GuzzleHttp\CLient(['verify' => false]);
+$response = $client->get('https://abstrusegoose.com');
+$html = $response->getBody()->getContents();
+$crawler = new \Symfony\Component\DomCrawler\Crawler($html);
+$imgEl = $crawler->filter('section img');
+
+var_dump($imgEl->attr('src'));
+var_dump($imgEl->attr('title'));
+var_dump($imgEl->attr('alt'));
